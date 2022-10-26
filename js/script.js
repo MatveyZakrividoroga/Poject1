75cdc9ec619d24c2a1da142860d99d96364be4a1
Mathew <matveyzed@mail.ru>
2022-10-26 12:18:14 +0300

    start project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-let number=5;
-number=10;
-console.log(number);
+const numberOfFilms=+prompt("Сколько фильмов вы посмотрели?","");
+const personalMovieDB={
+
+    count: numberOfFilms,
+    movies: {
+
+    },
+    actors: {
+
+    },
+    genres: [],
+    privat: false
+
+};
+const lastWF1=prompt("Один из последних просмотренных фильмов?","");
+const rate1=prompt("На сколько оцените его?","");
+const lastWF2=prompt("Один из последних просмотренных фильмов?","");
+const rate2=prompt("На сколько оцените его?","");
+personalMovieDB.movies[lastWF1]=rate1;
+personalMovieDB.movies[lastWF2]=rate2;
+//console.log(personalMovieDB);
